6aea1edd596ba575cbf95cbd1167949e73c694c0
Ola-Olawoyin <oola.olawoyin@gmailcom>
2023-02-09 21:35:53 +0000

    add changes

---
+++ test/features/demo/WebInteraction.feature
@@ -0,0 +1,12 @@
+Feature: Web Interaction
+
+    @demo
+    Scenario Outline: Run first demo feature
+        Given A web page is opened
+        Then Perform web Interaction
+
+
+        Examples:
+            | TestID    |
+            | WEB_TC002 |
+
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,5 +1,5 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
-import chai from "chai"
+import chai from "chai";
 
 Given(/^Google page is opened$/, async function () {
   browser.url("http://www.google.com");
@@ -15,12 +15,37 @@ When(/^Search with (.*)$/, async function (searchItem) {
 });
 
 Then(/^Click on the first search result$/, async function () {
-  let ele = await $(`<h3>`);
+  let ele = await $(
+    `//*[@id='rso']/div[1]/div/div/div/div/div/div/div/div[1]/a/h3`
+  );
   await ele.click();
 });
 
 Then(/^URL should match (.*)$/, async function (expectedURL) {
   console.log(`>> expected URL : ${expectedURL}`);
   let url = await browser.getUrl();
-chai.expect(url).to.equal(expectedURL);
+  chai.expect(url).to.equal(expectedURL);
+});
+
+Given(/^A web page is opened$/, async function () {
+  await browser.url("/inputs");
+  await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+  await browser.maximizeWindow();
+});
+
+let num = "12345";
+
+//let strNum = num.toString();
+
+Then(/^Perform web Interaction$/, async function () {
+  let ele = await $(`//*[@id="content"]/div/div/div/input`);
+  // ele.setValue(num);
+  await ele.click();
+  for (let i = 0; i < num.length; i++) {
+    let charNum = num.charAt(i);
+    await browser.pause(1000);
+    await browser.keys(charNum);
+  }
+
+  await browser.pause(7000);
 });
